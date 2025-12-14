19c1a581b8b1e9a66410dbddb57ac9a5256647c9
imaliaafiza-collab <imaliaafiza@gmail.com>
2025-12-14 14:47:35 +0700

    Praktikum F — Asymmetric Property Visibility (PHP 8.4)

---
+++ pertemuan-4/Asymmetric.php
@@ -0,0 +1,28 @@
+<?php
+class Book {
+    public function __construct(
+        public private(set) string $title,
+        public protected(set) string $author
+    ) {}
+}
+
+// =====================
+// Subclass
+class Ebook extends Book {
+    public function changeAuthor(string $newAuthor): void {
+        $this->author = $newAuthor; // ✅ BOLEH (protected setter)
+        // $this->title = "Judul Baru"; // ❌ ERROR (private setter)
+    }
+}
+
+// =====================
+// Penggunaan
+$bk = new Book(title: "How to PHP", author: "Ada");
+echo $bk->title . PHP_EOL;   // Bisa dibaca
+echo $bk->author . PHP_EOL;  // Bisa dibaca
+
+$ebook = new Ebook(title: "OOP PHP", author: "Budi");
+echo "Sebelum: " . $ebook->author . PHP_EOL;
+
+$ebook->changeAuthor("Sari");
+echo "Sesudah: " . $ebook->author . PHP_EOL;
